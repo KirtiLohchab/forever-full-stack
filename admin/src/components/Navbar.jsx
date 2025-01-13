1a88e45c930984e8aeed31edea009e4d8df42326
Kirti Lohchab <ahlawatkirti227@gmail.com>
2025-01-14 01:38:10 +0530

    changes

--- admin/src/components/Navbar.jsx
+++ admin/src/components/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = ({ setToken }) => {
     <div className="flex items-center py-2 px-[4%] justify-between">
       <img className="w-[max(10%,80px)]" src={assets.logo} alt="" />
       <button
-        onClick={() => setToken(" ")}
+        onClick={() => setToken("")}
         className="bg-gray-600 text-white px-5 py-2 sm:px-7 sm:py-2 rounded-full text-xs sm:text-sm"
       >
         Logout

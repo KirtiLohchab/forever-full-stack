3ba81738411a0fbb375a7862e34f5513d96d11c4
Kirti Lohchab <ahlawatkirti227@gmail.com>
2025-01-13 22:17:24 +0530

    react router dom

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import Navbar from "./components/Navbar";
 import SideBar from "./components/SideBar";
-import { Route, Routes } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import Add from "./pages/Add";

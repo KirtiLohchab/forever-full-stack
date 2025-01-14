ee19eef8bc7e1e518f77b8f63db6f9f5bfa70e8c
Kirti Lohchab <ahlawatkirti227@gmail.com>
2025-01-14 23:50:36 +0530

    update the app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,47 +1,41 @@
 import React from "react";
-import { lazy, Suspense } from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Collection from "./pages/Collection";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Product from "./pages/Product";
+import Cart from "./pages/Cart";
+import Login from "./pages/Login";
+import PlaceOrder from "./pages/PlaceOrder";
+import Order from "./pages/Order";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import SearchBar from "./components/SearchBar";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-
-const Home = lazy(() => import("./pages/Home"));
-const Collection = lazy(() => import("./pages/Collection"));
-const About = lazy(() => import("./pages/About"));
-const Contact = lazy(() => import("./pages/Contact"));
-const Product = lazy(() => import("./pages/Product"));
-const Cart = lazy(() => import("./pages/Cart"));
-const Login = lazy(() => import("./pages/Login"));
-const PlaceOrder = lazy(() => import("./pages/PlaceOrder"));
-const Order = lazy(() => import("./pages/Order"));
-const Verify = lazy(() => import("./pages/Verify"));
+import Verify from "./pages/Verify";
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <div className="px-4 sm:px[5vw] md:px-[7vw] lg:px-[9vw]">
-        <ToastContainer />
-        <Navbar />
-        <SearchBar />
-        <Suspense fallback={<div>Loading...</div>}>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/collection" element={<Collection />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="/cart" element={<Cart />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/order" element={<Order />} />
-            <Route path="/verify" element={<Verify />} />
-            <Route path="/place-order" element={<PlaceOrder />} />
-            <Route path="/product/:productId" element={<Product />} />
-          </Routes>
-        </Suspense>
-        <Footer />
-      </div>
-    </BrowserRouter>
+    <div className="px-4 sm:px[5vw] md:px-[7vw] lg:px-[9vw]">
+      <ToastContainer />
+      <Navbar />
+      <SearchBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/collection" element={<Collection />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/order" element={<Order />} />
+        <Route path="/verify" element={<Verify />} />
+        <Route path="/place-order" element={<PlaceOrder />} />
+        <Route path="/product/:productId" element={<Product />} />
+      </Routes>
+      <Footer />
+    </div>
   );
 };
 

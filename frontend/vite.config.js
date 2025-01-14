7822fc5737aff8ebd714a4ec5b76da6df7c8fe14
Kirti Lohchab <ahlawatkirti227@gmail.com>
2025-01-14 23:23:28 +0530

    update vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "react-router-dom": require.resolve("react-router-dom"),
+      "react-router-dom": import.meta.resolve("react-router-dom"),
     },
   },
   server: { port: 5173 },

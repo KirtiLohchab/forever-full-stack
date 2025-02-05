052b072279e1f69117e235c8745ca7f6462ef774
Kirti Lohchab <ahlawatkirti227@gmail.com>
2025-02-05 23:23:38 +0530

    package.lock.json

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,9 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      "react-router-dom": require.resolve("react-router-dom"),
-    },
-  },
+  // server: {
+  //   proxy: "https://forever-backend-ruddy.vercel.app",
+  // },
 });

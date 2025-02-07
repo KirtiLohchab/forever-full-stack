1c10c3bc6bd8ebbad84771a82fa0220d8e357f41
Kirti Lohchab <ahlawatkirti227@gmail.com>
2025-02-07 23:20:40 +0530

    adminAuth

--- Backend/middleware/adminAuth.js
+++ Backend/middleware/adminAuth.js
@@ -3,13 +3,15 @@ import jwt from "jsonwebtoken";
 const adminAuth = async (req, res, next) => {
   try {
     const { token } = req.headers;
+
     if (!token) {
       return res.json({ success: false, message: "Access denied" });
     }
     const token_decode = jwt.verify(token, process.env.JWT_SECRET);
-    if (token_decode !== process.env.ADMIN_Email + process.env.ADMIN_Password) {
+    if (token_decode !== process.env.ADMIN_EMAIL + process.env.ADMIN_PASSWORD) {
       return res.json({ success: false, message: "Access denied" });
     }
+
     next();
   } catch (error) {
     return res.json({ success: false, message: error.message });
